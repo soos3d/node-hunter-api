6ecab2d3f9c265359c8e69f515a9572e18afed9b
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-02-13 16:21:52 -0300

    add rate limiter and cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 const app = express();
 const testNodeLatency = require('./testNodeLatency');
 const endpointsList = require('./endpointsList');
 require('dotenv').config();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.PERMITTED_DOMAIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 25, // limit each IP to 25 requests per windowMs
+  message: "Too many requests, please try again later"
+});
+
+// apply the limiter to all requests
+app.use(limiter);
 
 let results = [];
 let lastRun = null;
@@ -28,6 +38,7 @@ app.get('/results', (req, res) => {
   res.send({ results, lastRun });
 });
 
+// Enable running on-demand tests
 /*
 app.get('/run-node', async (req, res) => {
   const index = req.query.index;
@@ -41,4 +52,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
