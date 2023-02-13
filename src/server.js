ecd57726a10b816e7258458349157b38facd5e86
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-02-13 14:51:17 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,16 +8,35 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
-  
-  app.get('/run-node', async (req, res) => {
-    const index = req.query.index;
-    const endpoint = endpointsList[index];
+});
+
+let results = [];
+
+const intervalId = setInterval(async () => {
+  results = [];
+  for (let i = 0; i < endpointsList.length; i++) {
+    const endpoint = endpointsList[i];
     const result = await testNodeLatency(endpoint);
-    res.send({ result });
-  });
+    results.push(result);
+    console.log(results)
+  }
+}, 300000); // 5 minutes in milliseconds
+
+app.get('/results', (req, res) => {
+  res.send({ results });
+});
+
+/*
+app.get('/run-node', async (req, res) => {
+  const index = req.query.index;
+  const endpoint = endpointsList[index];
+  const result = await testNodeLatency(endpoint);
+  res.send({ result });
+});
+*/
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
